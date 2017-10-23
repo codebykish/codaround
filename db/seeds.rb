241ac7e5f5b4cc613c29a5074a91fce6726201a3
codebykish <kishan_letters@yahoo.com>
2017-10-23 14:44:25 +0200

    Create Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+User.create!([
+  {name: "Wayne", email: "piet@piet1.com", password: 123456, admin: false},
+  {name: "Taddeo", email: "piet@piet2.com", password: 123456, admin: true},
+  {name: "Klaas", email: "piet@piet3.com", password: 123456, admin: false},
+  {name: "Erik", email: "piet@piet4.com", password: 123456, admin: false},
+  {name: "Arno", email: "piet@piet5.com", password: 123456, admin: false},
+  {name: "Mike", email: "piet@piet6.com", password: 123456, admin: false},
+  {name: "Rembert", email: "piet@piet7.com", password: 123456, admin: false},
+  {name: "Matt", email: "piet@piet8.com", password: 123456, admin: false},
+  {name: "Michiel", email: "piet@piet9.com", password: 123456, admin: false},
+  {name: "Cor", email: "piet@piet0.com", password: 123456, admin: false},
+  {name: "Ernest", email: "piet@piet10.com", password: 123456, admin: false},
+  {name: "Lara", email: "piet@piet11.com", password: 123456, admin: false},
+  {name: "Floris", email: "piet@piet12.com", password: 123456, admin: false},
+  {name: "Friso", email: "piet@piet13.com", password: 123456, admin: false},
+  {name: "Aljaz", email: "piet@piet14.com", password: 123456, admin: false},
+  {name: "Alex", email: "piet@piet15.com", password: 123456, admin: false},
+  {name: "Xander", email: "piet@piet16.com", password: 123456, admin: false},
+  {name: "Jhjdn", email: "piet@piet17.com", password: 123456, admin: false},
+  {name: "Anthony", email: "piet@piet18.com", password: 123456, admin: false},
+  {name: "Maurice", email: "piet@piet19.com", password: 123456, admin: false},
+  {name: "Bruna", email: "piet@piet20.com", password: 123456, admin: false},
+
+
+],
+
+)

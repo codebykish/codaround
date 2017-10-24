dda9f1996994855d7b90488b2e6134550532ce78
Cor Kroon <c.y.kroon@gmail.com>
2017-10-24 11:19:16 +0200

    Add toggleAdmin function

---
+++ app/assets/javascripts/students.js
@@ -0,0 +1,14 @@
+function updateCounters(){
+
+}
+
+function toggleAdmin() {
+  var checkbox = this;
+
+  $(checkbox).parent().toggleClass("admin");
+}
+
+$(document).ready(function() {
+  $("input[type=checkbox]").bind('change', toggleAdmin);
+  updateCounters();
+});

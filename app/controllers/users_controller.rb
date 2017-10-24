800cfbc2cd3510e4ca152428031f651541e70dcd
Cor Kroon <c.y.kroon@gmail.com>
2017-10-24 10:57:31 +0200

    Making a list of all students with possibility to make them admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,27 @@
 class UsersController < ApplicationController
 
   def index
+    redirect_to new_user_session_path if !user_signed_in?
     @users = User.all
     @user = current_user
   end
+
+  def update
+    find_user
+
+    if @found_user.update_attributes(user_params)
+      redirect_to users_path
+    else
+      render "edit",  notice: "Try again!"
+    end
+  end
+
+  private
+  def find_user
+    @found_user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :admin)
+end
 end
